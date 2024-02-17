8d3bb58059bba71ca1f35ac3ea86cbc218e8e8fa
namhkse <namhkse@gmail.com>
2024-02-18 00:53:42 +0700

    feat(chapter 2): add string concatenate func

---
+++ strcat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void strcat2(char[], char[]);
+
+int main(int argc, char const *argv[])
+{
+    char one[20] = "one";
+    char two[] = "two";
+    strcat2(one, two);
+
+    printf("%s\n", one);
+
+    return 0;
+}
+
+/* strcat: concatenate t to end of s; s must be big enough */
+void strcat2(char s[], char t[])
+{
+    int i = 0;
+    int j = 0;
+    while(s[i] != '\0')     /* find end of s */
+        i++;
+    while((s[i++] = t[j++]) != '\0');       /* copy t */
+}
+
